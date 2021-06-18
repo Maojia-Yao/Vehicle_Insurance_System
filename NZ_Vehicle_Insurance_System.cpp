b28615fab28d43fd4d194b4c73058101bee32731
adamts028 <adamts028@gmail.com>
2021-06-18 22:37:46 +1200

    Added policy registration function

--- NZ_Vehicle_Insurance_System.cpp
+++ NZ_Vehicle_Insurance_System.cpp
@@ -67,6 +67,7 @@ void customerRegistration();
 void customerLogin();
 void customerScreen();
 void adminScreen();
+void policyRegistration(int);
 
 int main()
 {
@@ -219,3 +220,57 @@ void customerLogin()
 	customerScreen();
 }
 
+void policyRegistration(int policy_num) {
+
+	Policy p;
+	fstream policies;
+	int temp_num = 100 + policy_num;
+	bool check = true;
+
+	policies.open("policies.dat", ios::in | ios::binary);
+
+	while (check == true)
+	{
+		check = false;
+		while (policies.read(reinterpret_cast<char*>(&p), sizeof(p)))
+		{
+			if (p.policy_no = temp_num)
+			{
+				cout << temp_num;
+				temp_num += 100;
+				check = true;
+			}
+		}
+	}
+
+	p.policy_no = temp_num;
+
+	policies.close();
+
+	cout << "\nEnter your first name : ";
+	cin >> p.first_name;
+	cout << "\nEnter your last name : ";
+	cin >> p.last_name;
+	cout << "\nEnter your date of birth (dd/mm/yyyy) : ";
+	cin >> p.dob;
+	cout << "\nEnter your gender : ";
+	cin >> p.gender;
+	cout << "\nEnter your phone number : ";
+	cin >> p.contact_no;
+	cout << "\nEnter your email address : ";
+	cin >> p.email;
+	cout << "\nEnter your home address : ";
+	cin >> p.address;
+	cout << "\nEnter your vehicle registration number : ";
+	cin >> p.vehicleRegNumber;
+	cout << "\nEnter your vehicle name : ";
+	cin >> p.vehicle_name;
+	cout << "\nEnter your vehicle model : ";
+	cin >> p.model;
+
+	policies.open("policies.dat", ios::out | ios::app | ios::binary);
+
+	policies.write(reinterpret_cast<char*>(&p), sizeof(p));
+
+	policies.close();
+}
